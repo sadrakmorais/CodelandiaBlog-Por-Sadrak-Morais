2e0a860037f83caba058fae7767c250ba03ed7f4
sadrakmorais <sadraksdk1@gmail.com>
2021-08-13 11:22:00 -0300

    Att

---
+++ script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'left',
+    distance: '10px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.container-post', { delay: 200 });
+ScrollReveal().reveal('.date', { delay: 300 });
+ScrollReveal().reveal('.title', { delay: 400 });
+ScrollReveal().reveal('.description', { delay: 500 });
